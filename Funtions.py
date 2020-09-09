5da9b5bcda18c2627064b10730b7dd3b51351997
ruot-nyak <nyak.g.ruot@gmail.com>
2020-09-08 19:32:27 -0700

    first commit

---
+++ Funtions.py
@@ -0,0 +1,37 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+call_1 = area(9, 8)
+
+call_2 = area(4, 2)
+
+print(call_1)
+print(call_2)
+
+def subtract(num1, num2):
+    subtract = num1 - num2
+    return result
+
+number_1 = subtract(5, 3)
+number_2 = subtract(6, 2)
+
+print(number_1)
+print(number_2)
+
+def divide(num1, num2):
+    divide = num1 / num2
+    return results
+
+def add(num1, num2):
+    add = num1 + num2
+    return results
+
+sum_1 = add(4, 4)
+sum_2 = add(9, 6)
+
+print(sum_1)
+print(sum_2)
